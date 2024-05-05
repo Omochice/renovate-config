23622f468afa7962d51547fe2239822ffca3c26b
Omochice <mochice.mls.ntl@gmail.com>
2024-05-05 22:41:33 +0900

    test: use it.each instead of for-loop

--- test/deno/deno-land.test.ts
+++ test/deno/deno-land.test.ts
@@ -58,18 +58,16 @@ describe("deno.land for import map", () => {
     },
   ] as const;
 
-  for (const testCase of testCases) {
-    it(testCase.title, () => {
-      const re = regexps[0].map((r) => new RegExp(r, "gm"));
-      const matches = re
-        .map((r) => Array.from(testCase.input.matchAll(r)).map((e) => e.groups))
-        .filter((match) => match.length !== 0)
-        .flat();
-      expect(matches.length).toBe(1);
-      expect(matches[0]?.currentValue).toBe(testCase.currentValue);
-      expect(matches[0]?.depName).toBe(testCase.depName);
-    });
-  }
+  it.each(testCases)("$title", ({ input, currentValue, depName }) => {
+    const re = regexps[0].map((r) => new RegExp(r, "gm"));
+    const matches = re
+      .map((r) => Array.from(input.matchAll(r)).map((e) => e.groups))
+      .filter((match) => match.length !== 0)
+      .flat();
+    expect(matches.length).toBe(1);
+    expect(matches[0]?.currentValue).toBe(currentValue);
+    expect(matches[0]?.depName).toBe(depName);
+  });
 });
 
 describe("deno.land for import map", () => {
@@ -118,16 +116,14 @@ describe("deno.land for import map", () => {
     },
   ] as const;
 
-  for (const testCase of testCases) {
-    it(testCase.title, () => {
-      const re = regexps[1].map((r) => new RegExp(r, "gm"));
-      const matches = re
-        .map((r) => Array.from(testCase.input.matchAll(r)).map((e) => e.groups))
-        .filter((match) => match.length !== 0)
-        .flat();
-      expect(matches.length).toBe(1);
-      expect(matches[0]?.currentValue).toBe(testCase.currentValue);
-      expect(matches[0]?.depName).toBe(testCase.depName);
-    });
-  }
+  it.each(testCases)("$title", ({ input, currentValue, depName }) => {
+    const re = regexps[1].map((r) => new RegExp(r, "gm"));
+    const matches = re
+      .map((r) => Array.from(input.matchAll(r)).map((e) => e.groups))
+      .filter((match) => match.length !== 0)
+      .flat();
+    expect(matches.length).toBe(1);
+    expect(matches[0]?.currentValue).toBe(currentValue);
+    expect(matches[0]?.depName).toBe(depName);
+  });
 });
